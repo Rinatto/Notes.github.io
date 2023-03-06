ba6f81544a52abc3cf1bc858fe594fa05d166175
Rinatto <88773953+Rinatto@users.noreply.github.com>
2023-03-06 19:38:45 +0300

    Add files via upload

--- Notes/js/app.js
+++ Notes/js/app.js
@@ -10,43 +10,42 @@ Create_Note.addEventListener('click', function () {
     div.innerHTML = ` 
     <div class="card">
     <label for="name">Название заметки:</label>
-    <textarea name="name" id="areaName" cols="30" rows="2" placeholder="Введите номер заметки"></textarea>
+    <textarea name="name" cols="30" rows="2" placeholder="Введите номер заметки"></textarea>
     <label for="text">Текст:</label>
-    <textarea name="text" id="area" cols="30" rows="10" placeholder="Введите текст"></textarea>
+    <textarea name="text" cols="30" rows="10" placeholder="Введите текст"></textarea>
     <!--<button>Редактировать заметку</button>-->
     <button class = "delete_Note button button-delete">Удалить заметку</button>
-</div>
+    </div>
     `
     const DeleteNote = div.querySelector('.delete_Note');
 
     DeleteNote.addEventListener('click', function (e) {
      div.remove();
-    // console.log('удаление следующей кнопки')
-  });
+    });
+
 
 
 return div;
+
 
   });
 
 
   Delete_Note_first.addEventListener('click', function (e) {
      Card.remove();
-     //console.log('удаление первой кнопки')
-     
   });
 
   
- 
 /*
   areaName.value = localStorage.getItem('areaName');
   areaName.oninput = () => {
   localStorage.setItem('areaName', areaName.value)
   };
 */
+
 /*
-  area.value = localStorage.getItem('area');
-  area.oninput = () => {
-  localStorage.setItem('area', area.value)
+  areaText.value = localStorage.getItem('areaText');
+  areaText.oninput = () => {
+  localStorage.setItem('areaText', areaText.value)
   };
 */
